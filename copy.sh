d2e27cbfb7d837c61d0f516257deb7abaef1f77c
Stef Piatek <s.piatek@ucl.ac.uk>
2024-10-17 17:10:56 +0100

    Loop through multiple containers

--- copy.sh
+++ copy.sh
@@ -2,5 +2,18 @@
 
 set -euxo pipefail
 
-azcopy login --tenant-id ${TENANT_ID}
-azcopy copy ${AZ_COPY_SOURCE} /data/samplefile.txt
+# Convert the comma-separated list of containers into an array
+IFS=',' read -r -a CONTAINERS_ARRAY <<< "$CONTAINERS"
+
+# Loop through each container
+for CONTAINER in "${CONTAINERS_ARRAY[@]}"; do
+    # Skip any container which already has a export zip file
+    if [ ! -f "/data-export/${CONTAINER}.zip" ]; then
+        # Download to container within docker mount
+        mkdir -p "/data/${CONTAINER}"
+        azcopy copy "https://${STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER}/?${SAS_CONTEXT}" "/data/${CONTAINER}" --recursive
+
+        # Zip to export mount
+        zip -r "/data-export/${CONTAINER}.zip" "/data/${CONTAINER}"
+    fi
+done
