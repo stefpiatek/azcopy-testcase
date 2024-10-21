99df52db2f011389c4b079393b9c884af65dc0f1
Stef Piatek <s.piatek@ucl.ac.uk>
2024-10-21 15:22:41 +0100

    Fixup

--- copy.sh
+++ copy.sh
@@ -8,12 +8,12 @@ IFS=',' read -r -a CONTAINERS_ARRAY <<< "$CONTAINERS"
 # Loop through each container
 for CONTAINER in "${CONTAINERS_ARRAY[@]}"; do
     # Skip any container which already has a export zip file
-    if [ ! -f "/data-export/${CONTAINER}.zip" ]; then
+    if [ ! -f "/export/${CONTAINER}.zip" ]; then
         # Download to container within docker mount, as this has more storage
         mkdir -p "/data/${CONTAINER}"
         azcopy copy "https://${STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER}/?${SAS_CONTEXT}" "/data/${CONTAINER}" --recursive
 
         # Zip to export mount
-        zip -r "/data-export/${CONTAINER}.zip" "/data/${CONTAINER}"
+        zip -r "/export/${CONTAINER}.zip" "/data/${CONTAINER}"
     fi
 done
